a9188bf5f2e124d9cc3006f3041485a2b1e44f12
João Victor <joaoaraujo.367@gmail.com>
2020-04-06 22:09:04 -0300

    Change pthread_t to local variable

--- questao2.c
+++ questao2.c
@@ -5,18 +5,18 @@
 typedef struct {
   int tid;
   int num;
-  pthread_t *threads;
 } thread_data_t;
 
 void *inverse_sequence(void *arg)
 {
   thread_data_t *data = (thread_data_t *) arg;
+  pthread_t thread;
 
   if (data->tid <= data->num && data->tid > 0) {
     data->tid++;
-    pthread_create(&data->threads[data->tid - 1], NULL, inverse_sequence, data);
+    pthread_create(&thread, NULL, inverse_sequence, data);
 
-    pthread_join(data->threads[data->tid - 1], NULL);
+    pthread_join(thread, NULL);
 
     data->tid--;
     printf("Olá, eu sou a thread %d\n", data->tid);
@@ -32,16 +32,16 @@ int main(int argc, char *argv[])
     exit(EXIT_FAILURE);
   }
 
+  pthread_t thread;
   int num_threads = atoi(argv[1]);
 
   thread_data_t *thread_data = (thread_data_t *) malloc(sizeof(thread_data_t));
-  thread_data->threads = (pthread_t *) malloc(sizeof(pthread_t) * num_threads);
   thread_data->tid = 1;
   thread_data->num = num_threads;
 
-  pthread_create(thread_data->threads, NULL, inverse_sequence, thread_data);
+  pthread_create(&thread, NULL, inverse_sequence, thread_data);
 
-  pthread_join(*thread_data->threads, NULL);
+  pthread_join(thread, NULL);
 
   thread_data->tid--;
   printf("Olá, eu sou a thread %d\n", thread_data->tid);
